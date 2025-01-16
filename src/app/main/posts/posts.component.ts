51b7e6e15a3610051921a03db5aee1e1508ea0b3
Meezan Shaikh <meezan@tradexa.co.in>
2025-01-16 22:08:44 +0530

    posts list view implemented

--- src/app/main/posts/posts.component.ts
+++ src/app/main/posts/posts.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { LoaderComponent } from "../../shared/components/loader/loader.component";
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { CommonRepositoryService } from '../../core/common/common-repository.service';
+import { ListParams } from '../../core/models/list-params.model';
+import { tap } from 'rxjs';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [LoaderComponent, NgIf, AsyncPipe, NgFor, RouterLink],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
 
+  #commonRepositoryService = inject(CommonRepositoryService);
+  params = new ListParams();
+  postsLoading = signal(true);
+  posts$ = this.#commonRepositoryService.getPosts(this.params).pipe(
+    tap(() => {
+      this.postsLoading.set(false)
+    })
+  )
 }
