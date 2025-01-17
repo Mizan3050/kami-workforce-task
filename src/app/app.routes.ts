33bdbc769658c2fed0a4bf1dd42d538d9ddb088f
Meezan Shaikh <mizansid888@gmail.com>
2025-01-17 08:27:30 +0530

    user detail component added, mobile ui updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,27 @@ export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     {
         path: 'dashboard',
-        loadChildren: () => import("./main/dashboard/dashboard.routes").then(m => m.routes)
+        loadChildren: () => import("./main/dashboard/dashboard.routes").then(m => m.routes),
+        title: 'Dashboard'
     },
     {
         path: 'posts',
-        loadChildren: () => import("./main/posts/posts.routes").then(m => m.routes)
+        loadChildren: () => import("./main/posts/posts.routes").then(m => m.routes),
+        title: 'Posts'
     },
     {
         path: 'albums',
-        loadChildren: () => import("./main/album/album.routes").then(m => m.routes)
+        loadChildren: () => import("./main/album/album.routes").then(m => m.routes),
+        title: 'Albums'
     },
     {
         path: 'photos',
-        loadChildren: () => import("./main/photos/photos.routes").then(m => m.routes)
+        loadChildren: () => import("./main/photos/photos.routes").then(m => m.routes),
+        title: 'Photos'
+    },
+    {
+        path: 'user',
+        loadComponent: () => import("./main/user-detail/user-detail.component").then(c => c.UserDetailComponent),
+        title: 'User Detail'
     }
 ];
